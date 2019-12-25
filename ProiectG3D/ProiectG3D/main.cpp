a743c19a0004d052861dee356d716f9747c284d4
Teodor Mihaescu <skindexgama@gmail.com>
2019-12-25 22:38:47 +0200

    Latest project modifications. All tests included in main.cpp.

--- ProiectG3D/ProiectG3D/main.cpp
+++ ProiectG3D/ProiectG3D/main.cpp
@@ -1,7 +1,51 @@
-#include <iostream>
 #include <GL/glew.h>
+#include "Display.h"
+#include "Shader.h"
+#include "Mesh.h"
+#include "Texture.h"
+#include "Transform.h"
+
+#define WINDOW_WIDTH 800
+#define WINDOW_HEIGHT 600
 
 int main()
 {
-	std::cout << "Test";
+	Display display(WINDOW_WIDTH, WINDOW_HEIGHT, "G3D - Tancodrom & Elicoptere");
+
+	Vertex vertecies[] = {
+		Vertex(glm::vec3(-0.5f,-0.5f,0),glm::vec2(0.0f,0.0f)),
+		Vertex(glm::vec3(0,0.5f,0),		glm::vec2(0.5f,1.0f)),
+		Vertex(glm::vec3(0.5f,-0.5f,0),	glm::vec2(1.0f,0.0f))
+	};
+	unsigned indices[] = { 0,1,2 };
+
+	Mesh mesh(vertecies, sizeof(vertecies) / sizeof(vertecies[0]), indices, sizeof(indices) / sizeof(indices[0]));
+	Mesh monkeyMesh("./resources/monkey3.obj");
+	Shader shader("./resources/testShader");
+	Texture texture("./resources/bricks.jpg");
+	Camera camera(glm::vec3(0, 0, -5), 70.0f, static_cast<float>(WINDOW_WIDTH) / WINDOW_HEIGHT, 0.01f, 1000.0f);
+	Transform transform;
+
+	float counter = 0.0f;
+
+	while (!display.IsClosed())
+	{
+		display.Clear(0.0f, 0.15f, 0.3f);
+
+		transform.GetPosition().x = sinf(counter);
+		transform.GetRotation().y = sinf(counter)*cosf(counter);
+		transform.GetScale().x = sinf(counter/2);
+		transform.GetScale().y = sinf(counter/2);
+		transform.GetScale().z = sinf(counter/2);
+
+		shader.Bind();
+		texture.Bind(0);
+		shader.Update(transform, camera);
+		monkeyMesh.Draw();
+
+		display.Update();
+		counter += 0.001f;
+	}
+
+	return 0;
 }
