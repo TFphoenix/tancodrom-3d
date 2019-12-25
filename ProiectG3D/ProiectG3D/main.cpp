eeb529127a1df8b4ebf3011a675ab27cf5afa165
Teodor Mihaescu <skindexgama@gmail.com>
2019-12-26 00:19:18 +0200

    Managed to rendered a low polly tank.

--- ProiectG3D/ProiectG3D/main.cpp
+++ ProiectG3D/ProiectG3D/main.cpp
@@ -20,23 +20,24 @@ int main()
 	unsigned indices[] = { 0,1,2 };
 
 	Mesh mesh(vertecies, sizeof(vertecies) / sizeof(vertecies[0]), indices, sizeof(indices) / sizeof(indices[0]));
-	Mesh monkeyMesh("./resources/monkey3.obj");
+	Mesh monkeyMesh("./resources/tank_1mesh.obj");
 	Shader shader("./resources/testShader");
 	Texture texture("./resources/bricks.jpg");
-	Camera camera(glm::vec3(0, 0, -5), 70.0f, static_cast<float>(WINDOW_WIDTH) / WINDOW_HEIGHT, 0.01f, 1000.0f);
+	Camera camera(glm::vec3(0, 0, -10), 70.0f, static_cast<float>(WINDOW_WIDTH) / WINDOW_HEIGHT, 0.01f, 1000.0f);
 	Transform transform;
 
-	float counter = 0.0f;
+	//float counter = 0.0f;
+	transform.GetRotation().y = 60;
 
 	while (!display.IsClosed())
 	{
 		display.Clear(0.0f, 0.15f, 0.3f);
 
-		transform.GetPosition().x = sinf(counter);
+		/*transform.GetPosition().x = sinf(counter);
 		transform.GetRotation().y = sinf(counter)*cosf(counter);
 		transform.GetScale().x = sinf(counter/2);
 		transform.GetScale().y = sinf(counter/2);
-		transform.GetScale().z = sinf(counter/2);
+		transform.GetScale().z = sinf(counter/2);*/
 
 		shader.Bind();
 		texture.Bind(0);
@@ -44,7 +45,7 @@ int main()
 		monkeyMesh.Draw();
 
 		display.Update();
-		counter += 0.001f;
+		//counter += 0.001f;
 	}
 
 	return 0;
