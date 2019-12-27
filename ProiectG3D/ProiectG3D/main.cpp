1ac38a2868b0af31e0ad03c407189412a1399d34
Teodor Mihaescu <skindexgama@gmail.com>
2019-12-27 11:04:01 +0200

    Rendered tank & terrain with different shaders.

--- ProiectG3D/ProiectG3D/main.cpp
+++ ProiectG3D/ProiectG3D/main.cpp
@@ -10,7 +10,7 @@ int main()
 {
 	Display display(Settings::WindowWidth, Settings::WindowHeight, "G3D - Tancodrom & Elicoptere");
 
-	Vertex planeVertices[] = {
+	Vertex terrainVertices[] = {
 		Vertex(glm::vec3(-100.0f,0.0f,-100.0f),	glm::vec2(0.5f,1.0f)),
 		Vertex(glm::vec3(100.0f,0.0f,-100.0f),	glm::vec2(1.0f,0.0f)),
 		Vertex(glm::vec3(-100.0f,0.0f,100.0f),	glm::vec2(0.0f,0.0f)),
@@ -19,36 +19,36 @@ int main()
 	unsigned indices[] = { 2,1,0,
 							1,2,3 };
 
-	Mesh planeMesh(planeVertices, sizeof(planeVertices) / sizeof(planeVertices[0]), indices, sizeof(indices) / sizeof(indices[0]));
+	// Meshes
+	Mesh terrainMesh(terrainVertices, sizeof(terrainVertices) / sizeof(terrainVertices[0]), indices, sizeof(indices) / sizeof(indices[0]));
 	Mesh tankMesh("./resources/models/tank_full.obj");
-	Shader shader("./resources/testShader");
+	// Shaders
+	Shader basicShader("./resources/basicShader");
+	Shader lambertShader("./resources/LambertsLightShader");
+	// Textures
 	Texture grassTexture("./resources/textures/grass.jpg");
-	Texture tankTexture("./resources/textures/dark_green.jpg");
+	Texture tankTexture("./resources/textures/green.jpg");
+	// Camera & Transform
 	Camera camera(glm::vec3(0, 10, -10), 70.0f, static_cast<float>(Settings::WindowWidth) / Settings::WindowHeight, 0.01f, 1000.0f);
 	display.SetCamera(camera);
 	Transform transform;
 
-	/*float counter = 0.0f;
-	transform.GetRotation().y = 60;*/
-
+	// Render loop
 	while (!display.IsClosed())
 	{
 		display.Clear(0.0f, 0.15f, 0.3f);
 
-		/*transform.GetPosition().x = sinf(counter);
-		transform.GetRotation().y = sinf(counter)*cosf(counter);*/
-
-		shader.Bind();
-		shader.Update(transform, camera);
-
+		basicShader.Bind();
+		basicShader.Update(transform, camera);
 		grassTexture.Bind(0);
-		planeMesh.Draw();
+		terrainMesh.Draw();
 
+		lambertShader.Bind();
+		lambertShader.Update(transform, camera);
 		tankTexture.Bind(0);
 		tankMesh.Draw();
 
 		display.Update();
-		/*counter += 0.001f;*/
 	}
 
 	return 0;
--- ProiectG3D/ProiectG3D/resources/basicShader.fs
+++ ProiectG3D/ProiectG3D/resources/basicShader.fs
@@ -8,5 +8,4 @@ uniform sampler2D diffuse;
 void main()
 {
 	gl_FragColor = texture2D(diffuse, texCoord0);
-		//* clamp(dot(-vec3(0, 0, 1), normal0), 0.0, 1.0);
 }
