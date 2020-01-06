a006c8f5100c05564a23db1ea1ecedd4df7b39f4
Alexandra Manole <alexandramanole1999@gmail.com>
2020-01-06 22:42:56 +0200

    Made a vector that stores all the objects and then drawn some vegetation around the helicopter landing pad.

--- ProiectG3D/ProiectG3D/main.cpp
+++ ProiectG3D/ProiectG3D/main.cpp
@@ -26,26 +26,68 @@ int main()
 	display.SetCamera(camera);
 
 	// Objects
-	Vegetation vegetation1(Vegetation::Type::BUSH,Transform(glm::vec3(30,0,150)));
-	Vegetation vegetation2(Vegetation::Type::FLOWER, Transform(glm::vec3(50,0,150)));
-	Vegetation vegetation3(Vegetation::Type::GRASS, Transform(glm::vec3(75,0,150)));
-	Terrain terrain;
-	Tank tank1(Tank::CLASSIC, Transform(glm::vec3(50, 0, -50)));
-	Tank tank2(Tank::DOUBLE, Transform(glm::vec3(50, 0, 50)));
-	Tank tank3(Tank::TURTLE, Transform(glm::vec3(-50, 0, -50)));
-	Helicopter helicopter(Helicopter::CLASSIC, Transform(glm::vec3(-20, 80, 0)));
-	HelicopterLandingPad landingPad;
-	BigSpotlight spotlight1(Transform(glm::vec3(-100, 0, 90), glm::vec3(0, glm::radians(-45.0f), 0)));
-	BigSpotlight spotlight2(Transform(glm::vec3(-200, 0, 90)));
-	BigSpotlight spotlight3(Transform(glm::vec3(-300, 0, 90), glm::vec3(0, glm::radians(45.0f), 0)));
-	BigSpotlight spotlight1r(Transform(glm::vec3(-100, 0, 210), glm::vec3(0, glm::radians(225.0f), 0)));
-	BigSpotlight spotlight2r(Transform(glm::vec3(-200, 0, 210), glm::vec3(0, glm::radians(180.0f), 0)));
-	BigSpotlight spotlight3r(Transform(glm::vec3(-300, 0, 210), glm::vec3(0, glm::radians(135.0f), 0)));
-	DirtStadium stadium(Transform(glm::vec3(-200, 0, 150)));
+	std::vector<Object*> objects;
+
+	//Vegetation
+	objects.push_back(new Vegetation(Vegetation::Type::GRASS, Transform(glm::vec3(15, 0, -21))));
+	objects.push_back(new Vegetation(Vegetation::Type::GRASS, Transform(glm::vec3(17, 0, -22))));
+	objects.push_back(new Vegetation(Vegetation::Type::GRASS, Transform(glm::vec3(7, 0, -23))));
+	objects.push_back(new Vegetation(Vegetation::Type::GRASS, Transform(glm::vec3(-3, 0, -23))));
+	objects.push_back(new Vegetation(Vegetation::Type::GRASS, Transform(glm::vec3(-7, 0, -23))));
+	objects.push_back(new Vegetation(Vegetation::Type::GRASS, Transform(glm::vec3(10, 0, -23))));
+	objects.push_back(new Vegetation(Vegetation::Type::FLOWER, Transform(glm::vec3(-8, 0, -25))));
+	objects.push_back(new Vegetation(Vegetation::Type::FLOWER, Transform(glm::vec3(-5, 0, -23))));
+	objects.push_back(new Vegetation(Vegetation::Type::FLOWER, Transform(glm::vec3(-10, 0, -24)))); 
+	objects.push_back(new Vegetation(Vegetation::Type::BUSH, Transform(glm::vec3(0, 0, -23))));
+	objects.push_back(new Vegetation(Vegetation::Type::BUSH, Transform(glm::vec3(20, 0, -23))));
+
+	objects.push_back(new Vegetation(Vegetation::Type::BUSH, Transform(glm::vec3(23, 0, 0))));
+	objects.push_back(new Vegetation(Vegetation::Type::GRASS, Transform(glm::vec3(23, 0, -10))));
+	objects.push_back(new Vegetation(Vegetation::Type::GRASS, Transform(glm::vec3(23, 0, -10))));
+	objects.push_back(new Vegetation(Vegetation::Type::GRASS, Transform(glm::vec3(21, 0, -10))));
+	objects.push_back(new Vegetation(Vegetation::Type::GRASS, Transform(glm::vec3(22, 0, -12))));
+	objects.push_back(new Vegetation(Vegetation::Type::GRASS, Transform(glm::vec3(23, 0, -16))));
+	objects.push_back(new Vegetation(Vegetation::Type::GRASS, Transform(glm::vec3(23, 0, -7))));
+	objects.push_back(new Vegetation(Vegetation::Type::GRASS, Transform(glm::vec3(23, 0, 5))));
+	objects.push_back(new Vegetation(Vegetation::Type::GRASS, Transform(glm::vec3(23, 0, 10))));
+
+	objects.push_back(new Vegetation(Vegetation::Type::BUSH, Transform(glm::vec3(-23, 0, -5))));
+	objects.push_back(new Vegetation(Vegetation::Type::BUSH, Transform(glm::vec3(-23, 0, 10))));
+	objects.push_back(new Vegetation(Vegetation::Type::FLOWER, Transform(glm::vec3(-23, 0, 3))));
+
+	objects.push_back(new Vegetation(Vegetation::Type::BUSH, Transform(glm::vec3(-10, 0, 23))));
+	objects.push_back(new Vegetation(Vegetation::Type::BUSH, Transform(glm::vec3(5, 0, 23))));
+	objects.push_back(new Vegetation(Vegetation::Type::GRASS, Transform(glm::vec3(-5, 0, 23))));
+	objects.push_back(new Vegetation(Vegetation::Type::FLOWER, Transform(glm::vec3(23, 0, -17))));
+
+
+
+	//Terrain
+	objects.push_back(new Terrain);
+
+	//Tank
+	objects.push_back(new Tank (Tank::CLASSIC, Transform(glm::vec3(50, 0, -50))));
+	objects.push_back(new Tank (Tank::DOUBLE, Transform(glm::vec3(50, 0, 50))));
+	objects.push_back(new Tank (Tank::TURTLE, Transform(glm::vec3(-50, 0, -50))));
+
+	//Helicopter
+	objects.push_back(new Helicopter(Helicopter::CLASSIC, Transform(glm::vec3(-20, 80, 0))));
+	objects.push_back(new HelicopterLandingPad);
+
+	//BigSpotlight
+	objects.push_back(new BigSpotlight (Transform(glm::vec3(-100, 0, 90), glm::vec3(0, glm::radians(-45.0f), 0))));
+	objects.push_back(new BigSpotlight (Transform(glm::vec3(-200, 0, 90))));
+	objects.push_back(new BigSpotlight (Transform(glm::vec3(-300, 0, 90), glm::vec3(0, glm::radians(45.0f), 0))));
+	objects.push_back(new BigSpotlight (Transform(glm::vec3(-100, 0, 210), glm::vec3(0, glm::radians(225.0f), 0))));
+	objects.push_back(new BigSpotlight (Transform(glm::vec3(-200, 0, 210), glm::vec3(0, glm::radians(180.0f), 0))));
+	objects.push_back(new BigSpotlight (Transform(glm::vec3(-300, 0, 210), glm::vec3(0, glm::radians(135.0f), 0))));
+
+	//Stadium
+	objects.push_back(new DirtStadium (Transform(glm::vec3(-200, 0, 150))));
 
 	// Load Time measure: ~1.8s, ~2.5s
-	std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
-	std::cout << "Load Time = " << std::chrono::duration_cast<std::chrono::milliseconds> (end - begin).count() / 1000.0f << "[seconds]" << std::endl;
+	//::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+	//std::cout << "Load Time = " << std::chrono::duration_cast<std::chrono::milliseconds> (end - begin).(count) / 1000.0f << "[seconds]" << std::endl;
 
 
 	// Render loop
@@ -53,33 +95,10 @@ int main()
 	{
 		display.Clear(0.0f, 0.15f, 0.3f);
 
-		//Vegetation
-		vegetation1.UpdateThenDraw(camera);
-		vegetation2.UpdateThenDraw(camera);
-		vegetation3.UpdateThenDraw(camera);
-		
-		// Terrain
-		terrain.UpdateThenDraw(camera);
-
-		// Tanks
-		tank1.UpdateThenDraw(camera);
-		tank2.UpdateThenDraw(camera);
-		tank3.UpdateThenDraw(camera);
-
-		// Helicopter
-		helicopter.UpdateThenDraw(camera);
-
-		// Buildings
-		landingPad.UpdateThenDraw(camera);
-		spotlight1.UpdateThenDraw(camera);
-		spotlight2.UpdateThenDraw(camera);
-		spotlight3.UpdateThenDraw(camera);
-		spotlight1r.UpdateThenDraw(camera);
-		spotlight2r.UpdateThenDraw(camera);
-		spotlight3r.UpdateThenDraw(camera);
-		stadium.UpdateThenDraw(camera);
-
-
+		for (auto& object : objects)
+		{
+		  object->UpdateThenDraw(camera);
+		}
 
 		display.Update();
 	}
